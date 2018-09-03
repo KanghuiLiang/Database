3e3dca292f56cc85d0d65f2d69ed1f364e1fa895
Kanghui Liang <Hui@Apple-Macintosh.local>
2018-09-03 15:09:32 +0700

    twoWays

--- insert_student.php
+++ insert_student.php
@@ -26,37 +26,40 @@ if ($link -> connect_error) {
     die('Could not connect: ' . $link->connect_error);
 }
 else{
-	// echo "Connected sucessfully";
+	echo "Connected sucessfully";
 }
 
 // //select database
 // $link = mysqli_select_db($link, "University");
 
-$sql = "INSERT INTO University.Student"."(sid, fname, mName,Lname,DOB,address,Phone,Major,deptCode)".
+$sql_insert = "INSERT INTO University.Student"."(sid, fname, mName,Lname,DOB,address,Phone,Major,deptCode)".
 "VALUES ('$sid', '$fname', '$Mname', '$Lname','$Dob','$address','$Phone','$major','$deptcode')"; 
 
 // $result = mysqli_query($link, $sql);
 
-if ($link->query($sql) === TRUE) {
+if ($link->query($sql_insert) === TRUE) {
     echo "New record created successfully<br>";
 } else {
-    echo "Error: " . $sql . "<br>" . $link->error;
+    echo "Error: " . $sql_insert . "<br>" . $link->error;
 }
 
 
-$sql_view = "SELECT SId, Fname, Mname,Lname FROM University.Student
-WHERE SId=".$_POST['id'];
+// $sql_view = "SELECT SId, Fname, Mname,Lname FROM University.Student
+// WHERE SId=".$_POST['id'];
 
-$result = $link -> query($sql_view);
+// $result = $link -> query($sql_view);
 
-if ($result -> num_rows > 0){
-	while($row = $result ->fetch_assoc()){
-		echo "id: ". $row["SId"]. " -Name: " .
-		$row ["Fname"]. " " . $row["Mname"]. " " . $row["Lname"] ."<br>";
-	}
-} else {
-	echo "0 results";
-}
+// if ($result -> num_rows > 0){
+// 	while($row = $result ->fetch_assoc()){
+// 		echo "id: ". $row["SId"]. " -Name: " .
+// 		$row ["Fname"]. " " . $row["Mname"]. " " . $row["Lname"] ."<br>";
+// 	}
+// } else {
+// 	echo "0 results";
+// }
+
 $link->close();
+
+include_once ('view_students.php');
 
 ?>
